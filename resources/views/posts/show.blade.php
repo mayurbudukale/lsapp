410fdb48c621496f699128e34592ffc33f33c670
Mayur Budukale <mayurbudukale@gmail.com>
2018-01-25 11:54:58 +0530

    Changed user edit access to other user posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,10 +8,13 @@
 </div>
 <hr>
 <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+ @if(Auth::user()->id == $post->user_id)
+ 
 <hr>
 <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST', 'class'=>'pull-right'])!!}
 {{Form::hidden('_method', 'DELETE')}}
 {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+@endif
 {!!Form::close()!!}
 @endsection
